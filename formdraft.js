c1af22876325f93947a2c8b2b65521361db8d08e
The Karan Pargaien <137596078+thekaranpargaie@users.noreply.github.com>
2023-06-24 11:30:02 +0530

    Add files via upload

---
+++ formdraft.js
@@ -0,0 +1,45 @@
+﻿// © 2022
+// Developed By: The Karan Pargaien
+// v1
+
+let formDraftConst = null;
+
+function saveFormData(formId = "", uniqueIdentifier = "") {
+  debugger;
+  if (formId !== "") {
+    const formData = new FormData(document.getElementById(formId));
+    const serializedData = JSON.stringify(Array.from(formData.entries()));
+    localStorage.setItem("draft_" + formId + uniqueIdentifier, serializedData);
+    console.log("Formdata drafted successfully");
+  }
+}
+
+function populateFormData(formId = "", uniqueIdentifier = "") {
+  debugger;
+  if (formId !== "") {
+    const formData = localStorage.getItem("draft_" + formId + uniqueIdentifier);
+    if (formData !== null && formData !== undefined) {
+      const formObj = document.forms[formId];
+      const parsedData = JSON.parse(formData);
+      if (formObj !== undefined && parsedData !== undefined && parsedData.length > 0) {
+        for (let i = 0; i < parsedData.length; i++) {
+          try {
+            formObj.elements[parsedData[i][0]].value = parsedData[i][1];
+          } catch (err) {
+            console.log("Error occurred in populateFormData method: " + err);
+          }
+        }
+      }
+    }
+  }
+}
+
+function startDraftingForm(formId = "", uniqueIdentifier = "") {
+  formDraftConst = setInterval(function () {
+    saveFormData(formId, uniqueIdentifier);
+  }, 2000);
+}
+
+function stopDraftingForm() {
+  clearInterval(formDraftConst);
+}
